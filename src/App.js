d1c93d7982db576664e2048dd6c6365e84b82b7e
Zoya <zosi71@mail.ru>
2022-09-08 21:24:49 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -14,32 +14,16 @@ const item = {
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="container">
-    <div className="background-element">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
     </div>
-    <div className="highlight-window">
-      <div className='highlight-overlay'></div>
-    </div>
-    <div className="window">
-      <ShopItemFunc item={item} />
-    </div>
-  </div>
   );
 }
 
